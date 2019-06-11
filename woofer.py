965c94e6122b69932c3d0a66361c8b49e2f30cf2
Nick Hanselman <nickhanselman@icloud.com>
2019-06-11 00:27:18 -0500

    testing urls in database

--- woofer.py
+++ woofer.py
@@ -2,6 +2,7 @@ import os
 import json
 import random
 import twitter
+import psycopg2
 from azure.cognitiveservices.search.imagesearch import ImageSearchAPI
 from msrest.authentication import CognitiveServicesCredentials
 
@@ -12,45 +13,41 @@ a_token_secret = os.environ["TWITTER_ACCESS_SECRET"]
 
 api = twitter.Api(c_key, c_secret, a_token_key, a_token_secret)
 
-image_index = int(os.environ["IMAGE_INDEX"])
+conn = psycopg2.connect(os.environ["DATABASE_URL"], sslmode='require')
+cur = conn.cursor()
 
-if (os.path.getsize("image_file.txt") == 0):
+cur.execute("SELECT * FROM index;")
+image_index = cur.fetchone()[0]
+
+cur.execute("SELECT COUNT(*) FROM images;")
+url_count = cur.fetchone()[0]
+
+if (url_count == 0 || url_count < image_index):
 
   print("FETCHING NEW URLS")
   client = ImageSearchAPI(CognitiveServicesCredentials(os.environ["BING_KEY"]))
 
   image_results = []
-  image_urls = []
   image_results.extend(client.images.search(query="cute dog").value)
   image_results.extend(client.images.search(query="puppies").value)
 
   for image in image_results:
-    image_urls.append(image.content_url)
+    cur.execute("INSERT INTO images(url) VALUES(%s)", image.content_url)
 
-  with open('image_file.txt', 'a') as image_file:
-    image_file.write(json.dumps(image_urls))
+  conn.commit()
 
-url_list = []
-with open('image_file.txt', 'r') as image_file:
-  url_list = json.load(image_file)
+# tweeted = False
 
-if (image_index > len(url_list)):
-  image_index = 0
+# while not tweeted:
+#   try:
+#     url = url_list[image_index]
+#     image_index += 1
+#     print("Attempting tweet of image url: {}".format(url))
+#     api.PostUpdate(". @CarterAlzamora @Houghelpuf", media=url)
+#     tweeted = True
+#   except twitter.error.TwitterError as err:
+#     print("Tweet failed with error: {}".format(err))
 
-tweeted = False
-
-while not tweeted:
-  try:
-    url = url_list[image_index]
-    image_index += 1
-    print("Attempting tweet of image url: {}".format(url))
-    api.PostUpdate(". @CarterAlzamora @Houghelpuf", media=url)
-    tweeted = True
-  except twitter.error.TwitterError as err:
-    print("Tweet failed with error: {}".format(err))
-
-print("Tweet successful")
-
-os.environ["IMAGE_INDEX"] = str(image_index)
+# print("Tweet successful")
 
 print("Process Complete")
